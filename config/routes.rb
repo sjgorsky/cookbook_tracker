74ae299846b6e66d447f60b4776d134eb229b4b3
Scott Gorsky <scottgorsky@Scott-Gorskys-MacBook-Pro.local>
2022-02-20 19:22:03 -0600

    add new book form

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,7 @@ Rails.application.routes.draw do
   get '/new',     to: 'books#new'
   post '/books',  to: 'books#create'
 
+  resources :books
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end

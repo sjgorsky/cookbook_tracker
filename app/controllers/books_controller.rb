bbeedd917c46edfff4e8d60baec47ec02131455d
Scott Gorsky <scottgorsky@Scott-Gorskys-MacBook-Pro.local>
2022-02-21 14:19:26 -0600

    connected create route

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -8,5 +8,18 @@ class BooksController < ApplicationController
   end
 
   def create
+    @book = Book.new(book_params)
+    if @book.save
+      flash[:info] = "Book successfully added to shelf."
+      redirect_to new_url
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+  def book_params
+    params.require(:book).permit(:title, :author, :rating)
   end
 end

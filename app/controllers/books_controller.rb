4e9e480bbdcf279b96098b16b5a205acb34fc225
Scott Gorsky <scottgorsky@Scott-Gorskys-MacBook-Pro.local>
2022-03-08 20:19:23 -0600

    delete action WIP

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -37,6 +37,13 @@ class BooksController < ApplicationController
     end
   end
 
+  def destroy
+    @book = Book.find(params[:id])
+    @book.destroy
+
+    redirect_to root_path, status: :see_other
+  end
+
   private
 
   def book_params
